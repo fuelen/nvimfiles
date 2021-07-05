6c248f88d03cfdc24c4f52fe24d110f9334b4fff
Artur Plysyuk <ifuelen@gmail.com>
2021-07-06 00:12:53 +0300

    Apply ivy theme for some telescope commands

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return function()
   vim.api.nvim_set_keymap("n", "<Leader>gs", [[<Cmd> Telescope git_status <CR>]], {noremap = true, silent = true})
   vim.api.nvim_set_keymap("n", "<Leader>cm", [[<Cmd> Telescope git_commits <CR>]], {noremap = true, silent = true})
-  vim.api.nvim_set_keymap("n", "<Leader>f", [[<Cmd> Telescope find_files <CR>]], {noremap = true, silent = true})
+  vim.api.nvim_set_keymap("n", "<Leader>f", [[<Cmd> Telescope find_files theme=get_ivy<CR>]], {noremap = true, silent = true})
   vim.api.nvim_set_keymap("n", "<Leader>lg", [[<Cmd> Telescope live_grep <CR>]], {noremap = true, silent = true})
 
   -- media_files plugin is not installed
@@ -11,7 +11,6 @@ return function()
   --     [[<Cmd>lua require('telescope').extensions.media_files.media_files()<CR>]],
   --     {noremap = true, silent = true}
   -- )
-  vim.api.nvim_set_keymap("n", "<Leader>b", [[<Cmd>Telescope buffers<CR>]], {noremap = true, silent = true})
-  vim.api.nvim_set_keymap("n", "<Leader>ht", [[<Cmd>Telescope help_tags<CR>]], {noremap = true, silent = true})
+  vim.api.nvim_set_keymap("n", "<Leader>b", [[<Cmd>Telescope buffers theme=get_ivy<CR>]], {noremap = true, silent = true})
   vim.api.nvim_set_keymap("n", "<Leader>of", [[<Cmd>Telescope oldfiles<CR>]], {noremap = true, silent = true})
 end
