c9651cb5d894125ca8c5ba68c2a8c97d32026e8a
Artur Plysyuk <ifuelen@gmail.com>
2023-01-19 21:51:18 +0200

    Remove useless CamelCaseMotion keymap
    I never use it

--- lua/plugins/camel_case_motion.lua
+++ lua/plugins/camel_case_motion.lua
@@ -2,5 +2,4 @@ return function()
     vim.api.nvim_set_keymap("", "w", "<Plug>CamelCaseMotion_w", {})
     vim.api.nvim_set_keymap("", "b", "<Plug>CamelCaseMotion_b", {})
     vim.api.nvim_set_keymap("", "e", "<Plug>CamelCaseMotion_e", {})
-    vim.api.nvim_set_keymap("", "ge", "<Plug>CamelCaseMotion_ge", {})
 end
