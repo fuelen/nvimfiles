939949d577d0c62dc0c5e1aad650ede6dd45d377
Artur Plysyuk <ifuelen@gmail.com>
2023-01-20 21:46:22 +0200

    Make BG for git blame virtual text the same as cursor BG
    and add italic style :)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return function()
     -- Set colorscheme
-    vim.cmd.colorscheme 'neodark'
+    vim.cmd.colorscheme "neodark"
 
     vim.cmd("highlight NvimTreeNormal guibg=#172932")
     vim.cmd("highlight NvimTreeIndentMarker guifg=" .. vim.g.terminal_color_13)
@@ -14,5 +14,13 @@ return function()
     vim.cmd("highlight Whitespace guifg=" .. vim.g.terminal_color_13)
 
     -- fixes https://github.com/nvim-treesitter/nvim-treesitter/issues/3689
-    vim.api.nvim_set_hl(0, "@symbol", { link = "Identifier" })
+    vim.api.nvim_set_hl(0, "@symbol", {link = "Identifier"})
+
+    -- set background for GitBlameVirtualText the same as background for CursorLine, as git blame virtual text
+    -- is visible only on hover which is always CursorLine. FG color is a color of comments, as Git blame plugin
+    -- uses Comment group by default
+    vim.cmd(
+        "highlight GitBlameVirtualText guibg=" ..
+            vim.g.terminal_color_8 .. " guifg=" .. vim.g.terminal_color_7 .. " cterm=italic gui=italic"
+    )
 end
