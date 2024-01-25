48a254684c122139f91a7cef9b1e38d054cbd2dc
Artur Plysyuk <ifuelen@gmail.com>
2024-01-25 13:14:34 +0200

    Fix symbols and modules highlighting after treesitter update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -15,7 +15,8 @@ return function()
     vim.cmd("highlight Whitespace guifg=" .. vim.g.terminal_color_13)
 
     -- fixes https://github.com/nvim-treesitter/nvim-treesitter/issues/3689
-    vim.api.nvim_set_hl(0, "@symbol", {link = "Identifier"})
+    vim.api.nvim_set_hl(0, "@string.special.symbol.elixir", {link = "Identifier"})
+    vim.api.nvim_set_hl(0, "@module", {link = "Type"})
 
     -- set background for GitBlameVirtualText the same as background for CursorLine, as git blame virtual text
     -- is visible only on hover which is always CursorLine. FG color is a color of comments, as Git blame plugin
