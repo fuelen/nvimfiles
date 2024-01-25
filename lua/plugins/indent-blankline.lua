c2d68f50e732628ef53aa289eadc13e5bccc6c4c
Artur Plysyuk <ifuelen@gmail.com>
2024-01-25 13:15:00 +0200

    Update indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,18 +1,44 @@
+-- return function()
+--     require("indent_blankline").setup {
+--         show_first_indent_level = false,
+--         char = "┊",
+--         char_blankline = "",
+--         filetype_exclude = {
+--             -- default
+--             "lspinfo",
+--             "packer",
+--             "checkhealth",
+--             "help",
+--             "man",
+--             "",
+--             -- custom
+--             "startify"
+--         }
+--     }
+-- end
+
 return function()
-    require("indent_blankline").setup {
-        show_first_indent_level = false,
-        char = "┊",
-        char_blankline = "",
-        filetype_exclude = {
-            -- default
-            "lspinfo",
-            "packer",
-            "checkhealth",
-            "help",
-            "man",
-            "",
-            -- custom
-            "startify"
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+
+    require("ibl").setup {
+        enabled = true,
+        indent = {char = "┊"},
+        scope = {enabled = false},
+        exclude = {
+            filetypes = {
+                "lspinfo",
+                "packer",
+                "checkhealth",
+                "help",
+                "man",
+                "gitcommit",
+                "TelescopePrompt",
+                "TelescopeResults",
+                "",
+                -- custom
+                "startify"
+            }
         }
     }
 end
