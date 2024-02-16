c2ffc65ebb5c11f4f9cf6fdb4adf55145f9831af
Artur Plysyuk <ifuelen@gmail.com>
2024-02-16 21:45:17 +0200

    Autocomplete only from visible buffers, as all buffers is not so performant
    I see freezes during typing :(

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -34,10 +34,14 @@ return function()
             {name = "nvim_lsp"},
             {
                 name = "buffer",
-                -- All buffers
+                -- Visible buffers
                 option = {
                     get_bufnrs = function()
-                        return vim.api.nvim_list_bufs()
+                        local bufs = {}
+                        for _, win in ipairs(vim.api.nvim_list_wins()) do
+                            bufs[vim.api.nvim_win_get_buf(win)] = true
+                        end
+                        return vim.tbl_keys(bufs)
                     end
                 }
             },
