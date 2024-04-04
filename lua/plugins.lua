4ccf05734b71811330a20ba5e0d41d34c668df12
Artur Plysyuk <ifuelen@gmail.com>
2024-04-04 20:27:09 +0300

    Add new dependency for neotest

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -217,6 +217,7 @@ return require("packer").startup(
         use {
             "nvim-neotest/neotest",
             requires = {
+                "nvim-neotest/nvim-nio",
                 "nvim-lua/plenary.nvim",
                 "nvim-treesitter/nvim-treesitter",
                 "antoinemadec/FixCursorHold.nvim",
